9a46925b31efee07685d7dcdab9ba4309dc7587b
Matt Thomson <red.cataclysm@gmail.com>
2016-03-03 12:44:55 -0500

    React up and running

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const React = require('react');
+const HelloApp = require('./components/HelloApp.js');
+
+React.render(
+  <HelloApp />,
+  document.getElementById('react')
+);
---
+++ js/components/HelloApp.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const React = require('react');
+
+let HelloApp = React.createClass({
+  render: () => {
+    return (
+      <div>hi</div>
+    );
+  }
+});
+
+module.exports = HelloApp;
